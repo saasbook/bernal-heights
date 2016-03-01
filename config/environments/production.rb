db5e6104b6ad78b6c48f9a3baaae9207f6ae9eb8
Candy Chang <candyichang@gmail.com>
2016-03-01 07:32:46 +0000

    changes to config and gems for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  #heroku: precompile assets at deploy time
+  config.assets.initialize_on_precompile = false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
