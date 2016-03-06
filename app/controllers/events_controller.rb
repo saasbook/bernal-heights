983cfb5817a38f187e37202621f7597a3ced9d90
joycetoh8 <joycetoh@berkeley.edu>
2016-03-06 23:49:15 +0000

    added validation to the model, added flash css and html, added strong params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,18 @@
 class EventsController < ApplicationController
+  def event_params
+    params.require(:event).permit(:description, :end_date, :end_time, :event_organizer, :name, :start_date, :start_time, :location, :creator_name, :creator_email)
+  end
   
   def index
   end
   
   def new
     @event = Event.new
+  end
+  
+  def create
+    @event = Event.create!(event_params)
+    flash[:notice] = "#{@event.name} was successfully created."
+    redirect_to events_path
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+    validates :name, presence: true
+    validates :start_date, presence: true
+    validates :start_time, presence: true
+    validates :location, presence: true
+    validates :creator_name, presence: true
+    validates :creator_email, presence: true
 end
