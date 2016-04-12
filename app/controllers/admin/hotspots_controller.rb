c325c5ec9c9eaeffd38156a3072c27b6f4e2b805
baoxie <baoxie@berkeley.edu>
2016-04-12 21:57:18 +0000

    cherry picked commit : edited routes to differentiate btw admin and public users hotspot flows
    Conflicts:
    	app/controllers/admin/hotspots_controller.rb

--- app/controllers/admin/hotspots_controller.rb
+++ app/controllers/admin/hotspots_controller.rb
@@ -1,3 +1,18 @@
 class Admin::HotspotsController < ApplicationController
   before_filter :is_admin
+
+  def index
+    @hotspots = Hotspot.all
+    @hash = Gmaps4rails.build_markers(@hotspots) do |hotspot, marker|
+      marker.lat hotspot.latitude
+      marker.lng hotspot.longitude
+      marker.infowindow hotspot.issue_type
+    end
+  end
+  
+  def destroy
+    @hotspot = Hotspot.find(params[:id])
+    @hotspot.destroy
+    redirect_to admin_hotspots_path
+  end
 end
