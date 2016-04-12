3814245e6e803f088d4fe5428c79e12fd8a8979f
joycetoh8 <joycetoh@berkeley.edu>
2016-04-12 16:50:33 +0000

    changed hotspot model to validate all required fields

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -1,6 +1,13 @@
 class Hotspot < ActiveRecord::Base
     validates :issue_type, presence: true
     validates :location, presence: true
+    validates :occurred_time, presence: true
+    validates :occurred_date, presence: true
+    validates :details, presence: true
+    validates :creator_name, presence: true
+    validates :creator_email, presence: true
+    validates :creator_number, presence: true
+    
     geocoded_by :location
     after_validation :geocode
 end
