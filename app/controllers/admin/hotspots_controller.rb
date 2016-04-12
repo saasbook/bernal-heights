9011c69b3460b0cebd9c3bd6a377dd36b9a93a80
baoxie <baoxie@berkeley.edu>
2016-04-12 21:57:18 +0000

    edited routes to differentiate btw admin and public users hotspot flows

--- app/controllers/admin/hotspots_controller.rb
+++ app/controllers/admin/hotspots_controller.rb
@@ -1,2 +1,24 @@
 class Admin::HotspotsController < ApplicationController
+  before_filter :is_admin
+  def is_admin
+    if !admin_signed_in?
+      flash[:notice] = "You must be an administrator to see this page"
+      redirect_to events_path
+    end
+  end
+  
+  def index
+    @hotspots = Hotspot.all
+    @hash = Gmaps4rails.build_markers(@hotspots) do |hotspot, marker|
+      marker.lat hotspot.latitude
+      marker.lng hotspot.longitude
+      marker.infowindow hotspot.issue_type
+    end
+  end
+  
+  def destroy
+    @hotspot = Hotspot.find(params[:id])
+    @hotspot.destroy
+    redirect_to admin_hotspots_path
+  end
 end
