4ee02a7784971f3b8657f82b4d8d62b5a5636c18
baoxie <baoxie@berkeley.edu>
2016-04-14 03:41:31 +0000

    admin filter by region works now

--- app/controllers/admin/hotspots_controller.rb
+++ app/controllers/admin/hotspots_controller.rb
@@ -1,5 +1,6 @@
 class Admin::HotspotsController < ApplicationController
   before_filter :is_admin
+  
   def is_admin
     if !admin_signed_in?
       flash[:notice] = "You must be an administrator to see this page"
@@ -7,8 +8,16 @@ class Admin::HotspotsController < ApplicationController
     end
   end
   
+  
   def index
-    @hotspots = Hotspot.all
+    @all_regions = Hotspot.all_regions
+    #@hotspots = Hotspot.all
+    @selected_regions = params[:regions] || {}
+    if @selected_regions == {}
+      @selected_regions = Hash[@all_regions.map {|region| [region, region]}]
+    end
+    @hotspots = Hotspot.where(region: @selected_regions.keys)
+    
     @hash = Gmaps4rails.build_markers(@hotspots) do |hotspot, marker|
       marker.lat hotspot.latitude
       marker.lng hotspot.longitude
