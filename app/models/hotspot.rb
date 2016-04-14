3eecba25edb50252255c0e5d6a16eba8fc6c7a88
joycetoh8 <joycetoh@berkeley.edu>
2016-04-14 17:27:23 +0000

    created issue model and hotspotissue model (through model for hotspot and issue). set up HMT associations between hotspot,issue,hotspotissue

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -1,4 +1,7 @@
 class Hotspot < ActiveRecord::Base
+    has_many :hotspotissues
+    has_many :issues, :through => :hotspotissues
+    
     validates :issue_type, presence: true
     validates :location, presence: true
     geocoded_by :location
