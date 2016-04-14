c94da13a572c68ee30667a14b6fc99929f81a271
Candy Chang <candyichang@gmail.com>
2016-04-14 21:35:09 +0000

    updating sessions controller to home route, changed vview to haml

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -19,7 +19,7 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
