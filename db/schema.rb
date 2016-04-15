8d5dd2f779016e79076c97b4802ed1bd5b81d4ba
Candy Chang <candyichang@gmail.com>
2016-04-15 20:25:15 +0000

    removed old features, updated admin dashboard page route to admin home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160415184949) do
 
   create_table "admins", force: :cascade do |t|
