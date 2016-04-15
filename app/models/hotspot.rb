d1123f34abad7746e04dffc83b0a397d7f97e897
joycetoh8 <joycetoh@berkeley.edu>
2016-04-15 19:19:08 +0000

    checkbox for issue types works now

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -2,7 +2,6 @@ class Hotspot < ActiveRecord::Base
     has_many :hotspotissues
     has_many :issues, :through => :hotspotissues
     
-    validates :issue_type, presence: true
     validates :location, presence: true
     validates :occurred_time, presence: true
     validates :occurred_date, presence: true
@@ -13,4 +12,17 @@ class Hotspot < ActiveRecord::Base
     
     geocoded_by :location
     after_validation :geocode
+    
+    def self.all_issues
+        ['Car Break-In', 'Abandoned Car','Broken Streetlight', 'Illegal Drug Transactions','Litter/Dumping Trash','Public Drinking and Noise','Other']
+    end
+    
+    def issue_types
+        issue_types = ""
+        self.issues.each do |issue|
+            issue_types = issue_types + issue.issue_type
+        end
+        issue_types
+    end
+      
 end
