91ed1b5a0c17833a4d5206a03f1b604933327e9d
Candy Chang <candyichang@gmail.com>
2016-04-15 21:45:17 +0000

    revised admin routing from navbar

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -5,6 +5,24 @@ class Admin::EventsController < EventsController
     @events = Event.where(approved: false)
   end
   
+  def new
+    super
+    render '/events/new'
+  end
+  
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      @event.confirm
+      flash[:notice] = "#{@event.name} was successfully created."
+      redirect_to admin_events_path
+    else 
+      # flash[:error] = @event.errors
+      flash.now[:warning] = "You have not filled out all required fields."
+      render :new
+    end
+  end
+  
   def confirm
     @event = Event.find(params[:event_id])
     @event.confirm
