d599412fc05067c19dabc5551e1e99b62e3ff220
Candy Chang <candyichang@gmail.com>
2016-04-30 07:51:47 +0000

    asset not showing in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,13 +26,15 @@ Rails.application.configure do
 
   #heroku: precompile assets at deploy time
   config.assets.initialize_on_precompile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
