d9590ede0c89cd7d6c6ec6833ddebfa41af3d2aa
Candy Chang <candyichang@gmail.com>
2016-05-02 19:13:55 +0000

    fixing migration to work with PG

--- db/migrate/20160502180118_default_valuesto_hotspots.rb
+++ db/migrate/20160502180118_default_valuesto_hotspots.rb
@@ -1,6 +1,6 @@
 class DefaultValuestoHotspots < ActiveRecord::Migration
   def change
-    change_column_default :hotspots, :walk, default: false
-    change_column_default :hotspots, :to_share, default: false
+    change_column_default :hotspots, :walk, false
+    change_column_default :hotspots, :to_share, false
   end
 end
